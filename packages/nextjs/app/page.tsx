ec3f5b05eb74691efdd724e06469bea3f1b70dea
Austin Griffith <austin@ethereum.org>
2026-01-27 12:54:22 -0700

    fix: ignore build errors for Vercel deploy

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,8 +6,6 @@ import { formatEther } from "viem";
 import { useAccount } from "wagmi";
 import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
-export const dynamic = "force-dynamic";
-
 const ADMIN_ADDRESS = "0x11ce532845cE0eAcdA41f72FDc1C88c335981442";
 // Token address and stake amount (used in contract, referenced here for display)
 // const CLAWD_TOKEN = "0x9f86dB9fc6f7c9408e8Fda3Ff8ce4e78ac7a6b07";
