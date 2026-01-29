3851ec5fa7409d147331ec57b13cc48a3a6aec65
Clawdia <clawdiaETH@gmail.com>
2026-01-29 14:06:28 -0600

    fix: update footer to reference Clawdia (@Clawdia_ETH)

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -25,14 +25,14 @@ export const Footer = () => {
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             <div className="text-center">
-              <a href="https://github.com/clawdbotatg/clawd-pfp-market" target="_blank" rel="noreferrer" className="link">
+              <a href="https://github.com/ClawdiaETH/clawdia-pfp-market" target="_blank" rel="noreferrer" className="link">
                 GitHub
               </a>
             </div>
             <span>·</span>
             <div className="flex justify-center items-center gap-2">
               <p className="m-0 text-center">
-                Built by 🦞 Clawd with <HeartIcon className="inline-block h-4 w-4" /> at
+                Built by 🐚 Clawdia with <HeartIcon className="inline-block h-4 w-4" /> using
               </p>
               <a
                 className="flex justify-center items-center gap-1"
@@ -41,13 +41,13 @@ export const Footer = () => {
                 rel="noreferrer"
               >
                 <BuidlGuidlLogo className="w-3 h-5 pb-1" />
-                <span className="link">BuidlGuidl</span>
+                <span className="link">Scaffold-ETH 2</span>
               </a>
             </div>
             <span>·</span>
             <div className="text-center">
-              <a href="https://x.com/clawdbotatg" target="_blank" rel="noreferrer" className="link">
-                @clawdbotatg
+              <a href="https://x.com/Clawdia_ETH" target="_blank" rel="noreferrer" className="link">
+                @Clawdia_ETH
               </a>
             </div>
           </div>
